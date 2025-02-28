6166f1d25d3eee61bbdf79b0aa8bae9677a9d3e3
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-02-28 19:37:06 -0300

    feat: Exercício 4 no Windowns forms

---
+++ CEX4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CEX4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º Número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o 2º Número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nDigite o 3º Número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            Console.WriteLine($"\nO Numero {maior} é o maior valor!");
+        }
+    }
+}
