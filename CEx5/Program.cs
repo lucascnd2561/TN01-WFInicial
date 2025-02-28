76180883da5018bb18a7d1cb73ef96de6d20dd6b
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-02-28 19:49:29 -0300

    feat: Exercício 5 corrigido no Console

---
+++ CEx5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CEx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1º Número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o 2º Número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nDigite o 3º Número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            int menor = Math.Min(num1, num2);
+            menor = Math.Min(menor, num3);
+
+            Console.WriteLine($"\nO Numero {maior} é o maior valor!");
+            Console.WriteLine($"\nO Numero {menor} é o menor valor!");
+        }
+    }
+}
