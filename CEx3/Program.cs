3aaaaef4a4377e06d08256bfa2ac0de3e858c218
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-02-27 22:26:24 -0300

    Ex03

---
+++ CEx3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1º número! ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDgite o 2º Número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\n0 Numero {maior} é o maior valor!");
+        }
+    }
+}
