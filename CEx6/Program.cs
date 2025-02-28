afa93d6bf32906f06c9f81467cf759c01296b3de
LUCAS DA SILVA CAMILO <lucas.scamilo2@senacsp.edu.br>
2025-02-28 20:19:21 -0300

    feat: Exercício 6 Corrigindo no Console

---
+++ CEx6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CEx6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o seu nome: ");
+            string nome = Console.ReadLine()!;
+            Console.Write("\nIforme o numero de horas trabalhadas: ");
+            int quantidaeHoras = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nIrforme o valor da hora: ");
+            double valorhora = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = valorhora * quantidaeHoras;
+
+            Console.WriteLine($"A renda total de {nome} é R$ {resultado:F2}");
+        }
+    }
+}
